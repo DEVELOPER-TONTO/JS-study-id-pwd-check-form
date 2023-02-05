0c4eb478de5658c868e5e1a755536fb4d7ed0e8b
DEVELOPER_TONTO <hellokywoo11@gmail.com>
2023-02-06 01:36:53 +0900

    아이디 비번 체크

--- form.js
+++ form.js
@@ -22,3 +22,20 @@
     $('.btn-danger').on('click',function(){
         $('.black-bg').removeClass('show-ani');
     });
+
+    //전송버튼 누르면 첫 input 값 공백씨 알림 띄움
+    document.querySelector('form').addEventListener('submit',function(e){
+        if(document.querySelector('.form-id').value == ''){
+            alert('아이디 뭐하냐 지금');
+            e.preventDefault();
+        }
+        if(document.querySelector('.form-pwd').value == ''){
+            alert('비번 뭐하냐 지금');
+            e.preventDefault();
+        }
+
+        if(document.querySelector('.form-pwd').value.length < 6){
+            alert('비번 6자넘게 지금');
+            e.preventDefault();
+        }
+    });
